9ef41ecc4a529f8dfda9608e0e8a1ec4f752b6f8
petitechatte <natalyavaskot@gmail.com>
2019-09-16 23:56:48 +0300

    Изменена форма облака для вывода статистики

--- js/stat.js
+++ js/stat.js
@@ -4,8 +4,20 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_DENT = 20;
 
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#ffffff';
-  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.beginPath();
+  ctx.moveTo(CLOUD_X, CLOUD_Y);
+  ctx.lineTo(CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + CLOUD_DENT);
+  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, CLOUD_Y);
+  ctx.lineTo(CLOUD_X + CLOUD_WIDTH - CLOUD_DENT, CLOUD_Y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, CLOUD_Y + CLOUD_HEIGHT);
+  ctx.lineTo(CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + CLOUD_HEIGHT - CLOUD_DENT);
+  ctx.lineTo(CLOUD_X, CLOUD_Y + CLOUD_HEIGHT);
+  ctx.lineTo(CLOUD_X + CLOUD_DENT, CLOUD_Y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(CLOUD_X, CLOUD_Y);
+  ctx.closePath();
+  ctx.fill();
 };
