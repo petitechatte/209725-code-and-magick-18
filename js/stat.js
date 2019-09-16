0f2fa1da06e0fea4e572f4eabb3677bdb523e4d8
petitechatte <natalyavaskot@gmail.com>
2019-09-17 00:18:23 +0300

    Введена функция для отрисовки облака или тени

--- js/stat.js
+++ js/stat.js
@@ -5,35 +5,28 @@ var CLOUD_Y = 10;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_DENT = 20;
+var CLOUD_COLOR = '#ffffff';
 var SHADOW_X = CLOUD_X + 10;
 var SHADOW_Y = CLOUD_Y + 10;
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + CLOUD_WIDTH / 2, y + CLOUD_DENT);
+  ctx.lineTo(x + CLOUD_WIDTH, y);
+  ctx.lineTo(x + CLOUD_WIDTH - CLOUD_DENT, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x + CLOUD_WIDTH, y + CLOUD_HEIGHT);
+  ctx.lineTo(x + CLOUD_WIDTH / 2, y + CLOUD_HEIGHT - CLOUD_DENT);
+  ctx.lineTo(x, y + CLOUD_HEIGHT);
+  ctx.lineTo(x + CLOUD_DENT, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x, y);
+  ctx.closePath();
+  ctx.fill();
+};
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.beginPath();
-  ctx.moveTo(SHADOW_X, SHADOW_Y);
-  ctx.lineTo(SHADOW_X + CLOUD_WIDTH / 2, SHADOW_Y + CLOUD_DENT);
-  ctx.lineTo(SHADOW_X + CLOUD_WIDTH, SHADOW_Y);
-  ctx.lineTo(SHADOW_X + CLOUD_WIDTH - CLOUD_DENT, SHADOW_Y + CLOUD_HEIGHT / 2);
-  ctx.lineTo(SHADOW_X + CLOUD_WIDTH, SHADOW_Y + CLOUD_HEIGHT);
-  ctx.lineTo(SHADOW_X + CLOUD_WIDTH / 2, SHADOW_Y + CLOUD_HEIGHT - CLOUD_DENT);
-  ctx.lineTo(SHADOW_X, SHADOW_Y + CLOUD_HEIGHT);
-  ctx.lineTo(SHADOW_X + CLOUD_DENT, SHADOW_Y + CLOUD_HEIGHT / 2);
-  ctx.lineTo(SHADOW_X, SHADOW_Y);
-  ctx.closePath();
-  ctx.fill();
-
-  ctx.fillStyle = '#ffffff';
-  ctx.beginPath();
-  ctx.moveTo(CLOUD_X, CLOUD_Y);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + CLOUD_DENT);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, CLOUD_Y);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH - CLOUD_DENT, CLOUD_Y + CLOUD_HEIGHT / 2);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH, CLOUD_Y + CLOUD_HEIGHT);
-  ctx.lineTo(CLOUD_X + CLOUD_WIDTH / 2, CLOUD_Y + CLOUD_HEIGHT - CLOUD_DENT);
-  ctx.lineTo(CLOUD_X, CLOUD_Y + CLOUD_HEIGHT);
-  ctx.lineTo(CLOUD_X + CLOUD_DENT, CLOUD_Y + CLOUD_HEIGHT / 2);
-  ctx.lineTo(CLOUD_X, CLOUD_Y);
-  ctx.closePath();
-  ctx.fill();
+  renderCloud(ctx, SHADOW_X, SHADOW_Y, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 };
