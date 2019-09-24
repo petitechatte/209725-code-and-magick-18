0c261a277572301fa486aacb4bf25853002d9915
petitechatte <natalyavaskot@gmail.com>
2019-09-24 16:03:04 +0300

    Добавлена функция вывода окна настроек персонажа

---
+++ js/setup.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+  var showSetupWindow = function () {
+    var setupWindow = document.querySelector('.setup');
+    setupWindow.classList.remove('hidden');
+  };
+
+  showSetupWindow();
+})();
