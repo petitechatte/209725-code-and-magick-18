2f3bf8492c4287f27068adfa937d5ca362387609
petitechatte <natalyavaskot@gmail.com>
2019-10-10 21:43:20 +0300

    Отрисовано сообщение об ошибке загрузки персонажей

--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -3,7 +3,18 @@
 'use strict';
 
 (function () {
-  var WIZARDS_NUMBER = 4; // число похожих персонажей в диалоговом окне настройки
+  // Число похожих персонажей в диалоговом окне настройки
+  var WIZARDS_NUMBER = 4;
+  // Параметры сообщения об ошибке
+  var ERROR_MESSAGE_HEADING = 'Хьюстон, у нас проблемы!'
+  var ERROR_MESSAGE_WIDTH = 100;
+  var ERROR_MESSAGE_HEIGHT = 100;
+  var ERROR_MESSAGE_PADDING = 20;
+  var ERROR_MESSAGE_BACKGROUND = 'white';
+  var ERROR_MESSAGE_TEXT_COLOR = 'black';
+  var ERROR_MESSAGE_HEADING_COLOR = 'red';
+  var ERROR_MESSAGE_FONT_WEIGHT = 'bold';
+  var ERROR_MESSAGE_TEXT_ALIGN = 'center';
 
   // Находим в разметке шаблон для персонажей и блок для их размещения
 
@@ -16,7 +27,7 @@
   window.similarWizards = {
     // Показываем блок с персонажами
     showSimilarWizards: function () {
-      window.backend.load(createSimilarWizards);
+      window.backend.load(createSimilarWizards, showErrorMessage);
       similarBlock.classList.remove('hidden');
     },
     // Удаляем персонажей при закрытии окна
@@ -54,5 +65,27 @@
     }
 
     similarWizardsList.appendChild(fragment);
+  };
+
+  // Сообщаем об ошибке загрузки персонажей
+  var showErrorMessage = function (errorMessage) {
+    // Стилизуем блок сообщения об ошибке
+    var errorNode = document.createElement('div');
+    errorNode.style.width = String(ERROR_MESSAGE_WIDTH) + '%';
+    errorNode.style.height = String(ERROR_MESSAGE_HEIGHT) + 'px';
+    errorNode.style.padding = String(ERROR_MESSAGE_PADDING) + 'px';
+    errorNode.style.background = ERROR_MESSAGE_BACKGROUND;
+    errorNode.style.color = ERROR_MESSAGE_TEXT_COLOR;
+    errorNode.style.fontWeight = ERROR_MESSAGE_FONT_WEIGHT;
+    errorNode.style.textAlign = ERROR_MESSAGE_TEXT_ALIGN;
+    errorNode.innerHTML = '<h3></h3>' + errorMessage;
+
+    // Добавляем заголовок
+    var errorHeading = errorNode.querySelector('h3');
+    errorHeading.style.color = ERROR_MESSAGE_HEADING_COLOR;
+    errorHeading.textContent = ERROR_MESSAGE_HEADING;
+
+    // Выводим сообщение
+    similarWizardsList.appendChild(errorNode);
   };
 })();
