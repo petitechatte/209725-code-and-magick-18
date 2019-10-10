d2bc4ea1222972539ac9523b9d37ad3bf3dc24ac
petitechatte <natalyavaskot@gmail.com>
2019-10-10 19:41:00 +0300

    Создан шаблон модуля для работы с сервером данных

---
+++ js/backend.js
@@ -0,0 +1,16 @@
+// Модуль работы с сервером данных
+
+'use strict';
+
+(function () {
+  window.backend = {
+    // Получение данных с сервера
+    load: function (onLoad, onError) {
+
+    },
+    // Отправление данных на сервер
+    save: function (data, onLoad, onError) {
+
+    }
+  };
+})();
