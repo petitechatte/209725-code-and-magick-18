c2c72bfa200c304242d579a8f3632e05bd41c9bf
petitechatte <natalyavaskot@gmail.com>
2019-10-13 19:04:18 +0300

    Реализована сортировка загруженных персонажей

--- js/settings.js
+++ js/settings.js
@@ -8,28 +8,16 @@
     COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
     EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
     FIREBALL_COLORS: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
+    // Скрытые поля, содержащие цвет плаща и глаз главного персонажа
+    heroCoatInput: window.utils.setupWindow.querySelector('input[name="coat-color"]'),
+    heroEyesInput: window.utils.setupWindow.querySelector('input[name="eyes-color"]')
   };
 
   // Находим элементы DOM
   var heroCoat = window.utils.setupWindow.querySelector('.wizard-coat');
   var heroEyes = window.utils.setupWindow.querySelector('.wizard-eyes');
   var fireball = window.utils.setupWindow.querySelector('.setup-fireball-wrap');
-  var heroCoatInput = window.utils.setupWindow.querySelector('input[name="coat-color"]');
-  var heroEyesInput = window.utils.setupWindow.querySelector('input[name="eyes-color"]');
   var fireballInput = window.utils.setupWindow.querySelector('input[name="fireball-color"]');
-
-  // Создаем объект для хранения текущих цветов персонажа
-  var currentLook = {
-    coatColor: '',
-    eyesColor: ''
-  };
-
-  // Запоминаем данные персонажа
-  var getMainWizardColors = function () {
-    currentLook.coatColor = heroCoatInput.value;
-    currentLook.eyesColor = heroEyesInput.value;
-    return currentLook;
-  };
 
   // Меняем по клику цвет для выбранного элемента персонажа
 
@@ -46,11 +34,11 @@
   var makeChangeable = function (element, input, properties) {
     element.addEventListener('click', function () {
       changeColor(element, input, properties);
-      currentLook = getMainWizardColors();
+      window.updateWizards();
     });
   };
 
-  makeChangeable(heroCoat, heroCoatInput, window.settings.COAT_COLORS);
-  makeChangeable(heroEyes, heroEyesInput, window.settings.EYES_COLORS);
+  makeChangeable(heroCoat, window.settings.heroCoatInput, window.settings.COAT_COLORS);
+  makeChangeable(heroEyes, window.settings.heroEyesInput, window.settings.EYES_COLORS);
   makeChangeable(fireball, fireballInput, window.settings.FIREBALL_COLORS);
 })();
--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -15,40 +15,18 @@
   var similarBlock = document.querySelector('.setup-similar');
   var similarWizardsList = window.utils.setupWindow.querySelector('.setup-similar-list');
 
-  // Создаем переменную для хранения данных с сервера
+  // Создаем переменные для хранения данных
   var loadedData = [];
+  var sortedWizards = [];
+  var mainWizardCurrentLook = {
+    coatColor: '',
+    eyesColor: ''
+  };
 
-  // Экспортируем функции для модуля работы диалогового окна
-
-  window.similarWizards = {
-    // Создаем персонажей
-    createSimilarWizards: function (wizardsData) {
-      var fragment = document.createDocumentFragment();
-      var newWizard;
-
-      // Используем try для защиты от некорректных данных с сервера
-      try {
-        for (var i = 0; i < WIZARDS_NUMBER; i++) {
-          newWizard = renderWizard(wizardsData[i]);
-          fragment.appendChild(newWizard);
-        }
-      } catch (err) {
-        showErrorMessage(err.message);
-      }
-
-      similarWizardsList.appendChild(fragment);
-    },
-    // Удаляем персонажей
-    removeSimilarWizards: function () {
-      similarWizardsList.innerHTML = '';
-    },
-    sortWizards: function (wizards) {
-      var mainWizardLook = window.settings.getMainWizardColors();
-      var mainWizardCoatColor = mainWizardLook.coatColor;
-      var mainWizardEyesColor = mainWizardLook.eyesColor;
-
-      getRank(wizard);
-    }
+  // Получаем настройки цвета главного персонажа
+  var getMainWizardColors = function () {
+    mainWizardCurrentLook.coatColor = window.settings.heroCoatInput.value;
+    mainWizardCurrentLook.eyesColor = window.settings.heroEyesInput.value;
   };
 
   // Устанавливаем текущий рейтинг каждого загруженного персонажа
@@ -57,19 +35,52 @@
     var rank = 0;
 
     // Назначаем баллы за основной критерий сходства - цвет плаща
-    if (wizard.colorCoat === mainWizardCoatColor) {
+    if (wizard.colorCoat === mainWizardCurrentLook.coatColor) {
       rank += 2;
     }
 
     // Назначаем дополнительные баллы за вторичный критерий сходства - цвет глаз
-    if (wizard.colorEyes === mainWizardEyesColor) {
+    if (wizard.colorEyes === mainWizardCurrentLook.eyesColor) {
       rank += 1;
     }
 
     return rank;
   };
 
-  window.similarWizards.sortWizards(loadedData);
+  // Сортируем персонажей по убыванию рейтинга
+
+  var sortWizards = function () {
+    // Используем try для защиты от некорректных данных с сервера
+    try {
+      var copiedWizards = loadedData.slice();
+
+      copiedWizards.sort(function (a, b) {
+        return getRank(b) - getRank(a);
+      });
+    } catch (err) {
+      showErrorMessage(err.message);
+    }
+    return copiedWizards;
+  };
+
+  // Создаем персонажей
+  var createSimilarWizards = function (wizardsData) {
+    var fragment = document.createDocumentFragment();
+    var newWizard;
+
+    for (var i = 0; i < WIZARDS_NUMBER; i++) {
+      newWizard = renderWizard(wizardsData[i]);
+      fragment.appendChild(newWizard);
+    }
+
+    similarWizardsList.appendChild(fragment);
+  };
+
+  // Удаляем персонажей
+
+  var removeSimilarWizards = function () {
+    similarWizardsList.innerHTML = '';
+  };
 
   // Создаем разметку для одного персонажа
 
@@ -87,6 +98,7 @@
   };
 
   // Сообщаем об ошибке загрузки персонажей
+
   var showErrorMessage = function (errorMessage) {
     // Создаем базовое сообщение
     var errorNode = window.utils.createMessage(ERROR_MESSAGE_HEADING, errorMessage);
@@ -101,11 +113,21 @@
   // Загружаем данные персонажей
 
   var getWizards = function (data) {
+    // Сохраняем данные
     loadedData = data;
     // Создаем похожих персонажей
-    window.similarWizards.createSimilarWizards(loadedData);
+    window.updateWizards();
     // Показываем блок с персонажами
     similarBlock.classList.remove('hidden');
+  };
+
+  // Обновляем список похожих персонажей
+
+  window.updateWizards = function () {
+    removeSimilarWizards();
+    getMainWizardColors();
+    sortedWizards = sortWizards();
+    createSimilarWizards(sortedWizards);
   };
 
   window.backend.load(getWizards, showErrorMessage);
